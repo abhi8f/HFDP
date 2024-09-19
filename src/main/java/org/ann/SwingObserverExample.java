2b5de3fdfbbca98176cbb26f14ed3bd6cec17c14
Abhishek N N <abhi8f@gmail.com>
2024-09-19 12:54:32 +0530

    lamda event

--- src/main/java/org/ann/SwingObserverExample.java
+++ src/main/java/org/ann/SwingObserverExample.java
@@ -2,8 +2,6 @@ package org.ann;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SwingObserverExample {
     JFrame frame;
@@ -16,8 +14,11 @@ public class SwingObserverExample {
     public void go() {
         frame = new JFrame();
         JButton button = new JButton("Should I do it?");
-        button.addActionListener(new AngelListener());
-        button.addActionListener(new DevilListener());
+        button.addActionListener(event ->
+                System.out.println("Don't do it, you might regret it!"));
+        button.addActionListener(event ->
+                System.out.println("Come on, do it!"));
+
         frame.getContentPane().add(BorderLayout.CENTER, button);
 
         // Set frame properties
@@ -25,17 +26,5 @@ public class SwingObserverExample {
         frame.getContentPane().add(BorderLayout.CENTER, button);
         frame.setSize(300, 300);
         frame.setVisible(true);
-    }
-
-    class AngelListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("Don't do it, you might regret it!");
-        }
-    }
-
-    class DevilListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("Come on, do it!");
-        }
     }
 }
