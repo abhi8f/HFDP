bc5feeb1ca6e24b0938b19654030b2a7c1440ca3
Abhishek N N <abhi8f@gmail.com>
2024-09-22 10:39:00 +0530

    tolowercase io

--- src/main/java/org/ann/Main.java
+++ src/main/java/org/ann/Main.java
@@ -1,17 +1,26 @@
 package org.ann;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int c;
+        try {
+            InputStream in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream("E:\\01\\01-HFDP\\src\\main\\java\\org\\ann\\test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
