948b26bc541281cd54343fa87148214d2bf3c6bc
Abhishek N N <abhi8f@gmail.com>
2024-09-22 10:45:16 +0530

    with size

--- src/main/java/org/ann/Main.java
+++ src/main/java/org/ann/Main.java
@@ -23,9 +23,17 @@ public class Main {
         System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
 
         Beverage beverage3 = new HouseBlend();
+        beverage3.setSize(Beverage.Size.TALL);
         beverage3 = new Soy(beverage3);
         beverage3 = new Mocha(beverage3);
         beverage3 = new Whip(beverage3);
         System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
+
+        Beverage beverage4 = new HouseBlend();
+        beverage4.setSize(Beverage.Size.VENTI);
+        beverage4 = new Soy(beverage4);
+        beverage4 = new Mocha(beverage4);
+        beverage4 = new Whip(beverage4);
+        System.out.println(beverage4.getDescription() + " $" + beverage4.cost());
     }
 }
--- src/main/java/org/ann/beverage/Beverage.java
+++ src/main/java/org/ann/beverage/Beverage.java
@@ -1,11 +1,24 @@
 package org.ann.beverage;
 
 public abstract class Beverage {
+    Size size = Size.TALL;
+
+
     String description = "Unknown Beverage";
 
     public String getDescription() {
         return description;
     }
 
+    public Size getSize() {
+        return this.size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
     public abstract double cost();
+
+    public enum Size {TALL, GRANDE, VENTI}
 }
